e1ac1423de87f5e92810e20538b289a8d7f32a69
CesarRD-98 <dedioscesar12@gmail.com>
2025-06-01 22:44:11 -0600

    update

--- Template_Proyecto/frontend/src/app/components/navbar/navbar.tsx
+++ Template_Proyecto/frontend/src/app/components/navbar/navbar.tsx
@@ -5,15 +5,12 @@ import { useRouter } from 'next/navigation'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 import NotificationBell from '../notificacionBell/notificacionBell';
+import UserIcon from '../userIcon/userIcon';
 
 
 export default function Navbar() {
 
     const router = useRouter()
-
-    function logout() {
-        router.push('/')
-    }
 
     return (
         <nav className={styles.navbar}>
@@ -24,7 +21,7 @@ export default function Navbar() {
                 <li><a href="/inicio">Tickets</a></li>
                 <li><a href="/reportes">Reportes</a></li>
                 <li><NotificationBell /></li>
-                <li onClick={logout}> Usuario <FontAwesomeIcon icon={faCircleUser} size='xl'/> </li>
+                <li><UserIcon /></li>
             </ul>
         </nav>
     )
