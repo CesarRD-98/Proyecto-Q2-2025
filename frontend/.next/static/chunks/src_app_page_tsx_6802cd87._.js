2378e6c045d10fab9c3bd9091c61a0fc356e255d
CesarRD-98 <dedioscesar12@gmail.com>
2025-06-04 11:57:19 -0600

    removed-files

--- frontend/.next/static/chunks/src_app_page_tsx_6802cd87._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_components_styles_loginForm_module_scss_module_8227aa06.css"
-],
-    source: "dynamic"
-});
